04a39ed3cdf0ad4fd41680d1290d97f5419d5802
Bardes Rémi <rbardes@a-018818.ups.u-psud.fr>
2019-04-11 19:17:07 +0200

    features jvous aime po

--- multiclass_perceptron.py
+++ multiclass_perceptron.py
@@ -1,5 +1,8 @@
 import json
+from data_analyseur import *
 from collections import defaultdict
+from operator import itemgetter
+
 
 class Perceptron:
 
@@ -111,11 +114,6 @@ class Perceptron:
         self._last_update = None
 
 #Loading data
-train_set = json.load(open("fr.ud.train.json"))
-test_set = json.load(open("fr.ud.test.json"))
-dev = json.load(open("fr.ud.dev.json"))
-foot_set = json.load(open("foot.json"))
-minecraft_set = json.load(open("minecraft.json"))
 
 #creating all labels for the loaded data
 def all_labels(datasets):
@@ -123,7 +121,7 @@ def all_labels(datasets):
     # qui transforme une liste de liste de liste etc en une simple liste c'est plus opti
     return set([label for dataset in datasets for words, labels in dataset for label in labels ])
 
-label_set = all_labels([train_set, test_set, dev, foot_set, minecraft_set])
+#label_set = all_labels([train_set, test_set, dev, foot_set, minecraft_set])
 
 #Generate a feature sparse vector (as a dictionnary) from a sentence, and a word position. 
 def feature_from_word(s,i):
@@ -166,9 +164,142 @@ def feature_from_word(s,i):
             res['wi+2: {}'.format(s[i+2])] = 1
     return res
 
-perceptron = Perceptron(list(label_set))
+#perceptron = Perceptron(list(label_set))
 
 
+#Generate a feature sparse vector (as a dictionnary) from a sentence, and a word position. 
+
+#Only the word as feature
+def features1(s,i):
+    res = {}
+    res['Word: {}'.format(s[i])] = 1
+    return res
+
+"""-------------------------------------------------------------------------------"""
+"""-------------------------------------------------------------------------------"""
+"""-------------------------------------------------------------------------------"""
+
+#The word, with the 2 before and after him in the sentence
+def features2(s,i):
+    res = features1(s,i)
+    res['Word: {}'.format(s[i])] = 1
+    if i>=2 and i< len(s)-2 :
+        res['wi-1: {}'.format(s[i-1])] = 1
+        res['wi-2: {}'.format(s[i-2])] = 1
+        res['wi+1: {}'.format(s[i+1])] = 1
+        res['wi+2: {}'.format(s[i+2])] = 1
+    else :
+        if i<1 :
+            res['wi-1: '] = 1
+            res['wi-2: '] = 1
+        elif i<2 :
+            res['wi-1: {}'.format(s[i-1])] = 1
+            res['wi-2: '] = 1
+        else :
+            res['wi-1: {}'.format(s[i-1])] = 1
+            res['wi-2: {}'.format(s[i-2])] = 1
+        if i>=len(s)-1 :
+            res['wi+1: '] = 1
+            res['wi+2: '] = 1
+        elif i>=len(s)-2 :
+            res['wi+1: {}'.format(s[i+1])] = 1
+            res['wi+2: '] = 1
+        else :
+            res['wi+1: {}'.format(s[i+1])] = 1
+            res['wi+2: {}'.format(s[i+2])] = 1
+    return res
+
+"""-------------------------------------------------------------------------------"""
+"""-------------------------------------------------------------------------------"""
+"""-------------------------------------------------------------------------------"""
+
+def words_frequency_from_corpus (train, test, n):
+    word_frequency = defaultdict(int)
+    for s, label in train:
+        for word in s :
+            word_frequency.update({word : word_frequency[word]+1 })
+    for s, label in test:
+        for word in s :
+            word_frequency.update({word : word_frequency[word]+1 })
+    return [i[0] for i in sorted(word_frequency.items(), key=itemgetter(1), reverse=True)[:n]]
+
+def distrib_features_dict(train,test):
+    freq = words_frequency_from_corpus(train,test, 10)
+    d_left = defaultdict(lambda:defaultdict(int))
+    d_right = defaultdict(lambda:defaultdict(int))
+    for s, label in train :
+        for i in range(len(s)):
+            #word = s[i]
+            #right_word = s[i+1]
+            #left_word = s[i-1]
+            if i == 0 and i < len(s)-1:
+                if s[i+1] in freq:
+                    d_right[s[i]][s[i+1]] += 1
+            elif i == len(s)-1 :
+                if s[i-1] in freq:
+                    d_left[s[i]][s[i-1]] += 1
+                else :
+                    d_left[s[i]]["a trash"] += 1
+            else:
+                if s[i-1] in freq:
+                    d_left[s[i]][s[i-1]] += 1
+                else :
+                    d_left[s[i]]["a trash"] += 1
+                if s[i+1] in freq:
+                    d_right[s[i]][s[i+1]] += 1
+    for s, label in test :
+        for i in range(len(s)):
+            #word = s[i]
+            #right_word = s[i+1]
+            #left_word = s[i-1]
+            if i == 0 and i < len(s)-1:
+                if s[i+1] in freq:
+                    d_right[s[i]][s[i+1]] += 1
+            elif i == len(s)-1 :
+                if s[i-1] in freq:
+                    d_left[s[i]][s[i-1]] += 1
+                else :
+                    d_left[s[i]]["a trash"] += 1
+            else:
+                if s[i-1] in freq:
+                    d_left[s[i]][s[i-1]] += 1
+                else :
+                    d_left[s[i]]["a trash"] += 1
+                if s[i+1] in freq:
+                    d_right[s[i]][s[i+1]] += 1
+
+    return (d_left,d_right)
+
+
+#The features described in "FLORS : Fast and Simple Domain Adaptation for PoS Tagging"
+def features3(s,i, l_features, r_features):
+    res = {}
+
+    #Left features
+    for ci, count in l_features[s[i]].items():
+        res["l_" + ci + "_" + s[i]] = 1 + math.log(count)
+    #Right features
+    for ci, count in r_features[s[i]].items():
+        res["r_" + s[i] + "_" + ci] = 1 + math.log(count)
+    
+    #Shape features
+
+    #Suffix features
+    for c in range(len(s[i])):
+        res['last'+ str(c+1) +'c: {}'.format(s[i][-(c+1):])] = 1
+
+    #shape features
+    return res
+
+train = train_datasets[0][1][:1000]
+test = test_datasets[0][1][:1000]
+l_feat, r_feat = distrib_features_dict(train,test)
+
+
+
+for ci, count in features3(train[1][0], 5, l_feat, r_feat).items():
+    print(ci + " = " + str(count))
+print("-------------")
 
 """
 #Make a dictionnary that associate every word & character to a unique identifier
@@ -186,7 +317,7 @@ def make_dicts(datasets):
 """
 
 #Training
-count = 0
+"""count = 0
 for epoch in range(10):
     for words, labels in train_set:
         for i in range(len(words)):
@@ -196,17 +327,17 @@ for epoch in range(10):
             perceptron.update(labels[i],prediction,features)
             count += 1
             if count%1000==0:
-                print(count)
+                print(count)"""
 
 #testing
-error = 0
+"""error = 0
 total_test = 0
 for entry in foot_set:
     for i in range (len(entry[0])):
         error += (perceptron.predict(feature_from_word(entry[0],i)) != entry[1][i])
         total_test += 1
 
-print(error*100/total_test)
+print(error*100/total_test)"""
 
 """
 Idée pour  évaluer le perceptron :
